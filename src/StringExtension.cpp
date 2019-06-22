3cbcf6ac5299ffd248d103b3de1388c0520ec366
Luchev <luchevz@gmail.com>
2019-06-23 02:35:14 +0300

    Feat StringExtension - additional string manipulation
    Add trim left/right/left&right functions for std::string

---
+++ src/StringExtension.cpp
@@ -0,0 +1,23 @@
+#include <src/StringExtension.h>
+#include <string>
+#include <algorithm>
+
+void StringExtension::trimLeft(std::string * string) {
+    string->erase(
+        string->begin(),
+        std::find_if(string->begin(), string->end(), [](int ch) {
+            return !std::isspace(ch);
+    }));
+}
+
+void StringExtension::trimRight(std::string * string) {
+    string->erase(
+        std::find_if(string->rbegin(), string->rend(), [](int character) {
+            return !std::isspace(character);
+    }).base(), string->end());
+}
+
+void StringExtension::trim(std::string * string) {
+    trimLeft(string);
+    trimRight(string);
+}
---
+++ src/StringExtension.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+class StringExtension {
+ public:
+    static void trimLeft(std::string * string);
+    static void trimRight(std::string * string);
+    static void trim(std::string * string);
+};
