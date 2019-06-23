f51d109aaa56b30774c084603c96678af66ec8c5
Luchev <luchevz@gmail.com>
2019-06-23 03:21:22 +0300

    Feat XmlAttribute - class for XML attributes
    Add XML Attribute class to hold an xml attribute in the following
    format: name = "value"

---
+++ src/XmlAttribute.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <src/XmlTag.h>
+#include <string>
+#include <vector>
+
+class XmlAttribute {
+ public:
+    XmlAttribute(const std::string & name, const std::string & value);
+
+    std::string getName() const;
+    std::string getValue() const;
+
+    std::string toString() const;
+    std::string toStringCompact() const;
+ private:
+    std::string name;
+    std::string value;
+};
